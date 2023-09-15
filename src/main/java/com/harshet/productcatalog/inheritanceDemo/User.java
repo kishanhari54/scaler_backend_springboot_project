c9e49b768753cf07aef78a189a4266bcb51922dd
Harshet <d.harshet@nagarro.com>
2023-09-15 22:04:01 +0530

    Single Table

--- src/main/java/com/harshet/productcatalog/inheritanceDemo/User.java
+++ src/main/java/com/harshet/productcatalog/inheritanceDemo/User.java
@@ -1,11 +1,14 @@
 package com.harshet.productcatalog.inheritanceDemo;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import lombok.Data;
 
 @Data
-@MappedSuperclass
+@Entity(name = "single_table_user")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class User {
     @Id
     private Long id;
